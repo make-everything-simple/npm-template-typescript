6af197f065b51158ef79e56cd162e787acb98f8e
Duy Le Ngoc <askbills@gmail.com>
2024-07-08 00:53:45 +0700

    refactor: update build support module type

---
+++ tests/dummy.test.ts
@@ -0,0 +1,7 @@
+// Dummy test as placeholder to comply with npm run test
+// We can keep this file if you are not go with TDD approach first
+
+test('Dummy test multiply two numbers correctly', () => {
+    const result = 2 * 3;
+    expect(result).toBe(6);
+});
